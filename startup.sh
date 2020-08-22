f9711ee68760b8c2785ce94096555c869df4a4b9
Gabe M <gabrielmeola@workfront.com>
2020-08-22 14:12:36 -0600

    chore: git reset

--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@
 # Get script dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Update self
+(cd $DIR && git fetch && git reset --hard origin/master)
+
 # Symlink system service
 ln -s "${DIR}/bearcode.service" /etc/systemd/system/bearcode.service || echo "Service already symlinked"
 
