5c5e9ca862655bce0a099d96f149bdea00dc9b89
Gabe M <gabrielmeola@workfront.com>
2020-08-22 14:46:16 -0600

    chore: turn off auth

--- daemon.sh
+++ daemon.sh
@@ -6,4 +6,4 @@ set -e
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --version=3.4.1
 
 # Start code-server
-code-server --bind-addr=0.0.0.0 --port=80
+code-server --bind-addr=0.0.0.0 --port=80 --auth none
